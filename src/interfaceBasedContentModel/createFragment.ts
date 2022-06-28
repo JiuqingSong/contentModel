2ab3ddb3596c1c547cd3aec2c0f81da4afe13cfd
Jiuqing Song <jisong@microsoft.com>
2022-06-28 16:54:36 -0700

    optimization: remove SPAN that has no attributes

--- src/interfaceBasedContentModel/createFragment.ts
+++ src/interfaceBasedContentModel/createFragment.ts
@@ -251,6 +251,28 @@ function optimize(root: Node) {
         }
     }
 
+    for (let child = root.firstChild; child; ) {
+        if (
+            isNodeOfType(child, NodeType.Element) &&
+            child.tagName == 'SPAN' &&
+            child.attributes.length == 0
+        ) {
+            const node = child;
+            let refNode = child.nextSibling;
+            child = child.nextSibling;
+
+            while (node.lastChild) {
+                const newNode = node.lastChild;
+                root.insertBefore(newNode, refNode);
+                refNode = newNode;
+            }
+
+            root.removeChild(node);
+        } else {
+            child = child.nextSibling;
+        }
+    }
+
     for (let child = root.firstChild; child; child = child.nextSibling) {
         optimize(child);
     }
